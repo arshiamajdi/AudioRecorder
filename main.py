33a88110e22bf7966ee5484d0be299670b6f10f3
Arshia <arshiamajdi4@gmail.com>
2025-12-15 23:51:13 -0800

    Finished combining audio and video with ffmpeg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import myAudio
 import screen
 import threading
+import ffmpeg
 
 t1 = threading.Thread(target=myAudio.start_AudioRecording, daemon=True)
 t2 = threading.Thread(target=screen.star_ScreenRecording, daemon=True)
@@ -11,3 +12,10 @@ t1.join()
 t2.join()
 
 print("Recording Completed.")
+
+input_video = ffmpeg.input('Recording.mp4')
+
+input_audio = ffmpeg.input('recorderFile.wav')
+
+ffmpeg.concat(input_video, input_audio, v=1, a=1).output('finished_video.mp4').run()
+
