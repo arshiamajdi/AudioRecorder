c5621062e37ac6beb3f01a1ab4eb9b8505798c77
Arshia <arshiamajdi4@gmail.com>
2025-12-15 20:34:24 -0800

    Turned audio.py and screen.py into functions and runs fine

--- audio.py
+++ audio.py
@@ -1,60 +1,52 @@
 import pyaudio
 import wave
-# import keyboard
-# import time
-
 from pynput.keyboard import Key, Listener
 
-stop_flag = False
+stop_AudioFlag = False
 def keyPressed(key):
     if key == Key.space:
         print("Recording Stopped.")
-        global stop_flag 
-        stop_flag = True
+        global stop_AudioFlag 
+        stop_AudioFlag = True
         return False
 
+def start_AudioRecording():
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 1024
+    OUTPUT_FILENAME = "recorderFile.wav"
 
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 1024
-OUTPUT_FILENAME = "recorderFile.wav"
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                        input=True, frames_per_buffer=CHUNK)
 
-audio = pyaudio.PyAudio()
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE,
-                    input=True, frames_per_buffer=CHUNK)
+    frames = []
 
-frames = []
-# print("Press 'space' to start recording audio.")
-# keyboard.wait('space')
-# print('Recording... Press "space" to stop.')
-# time.sleep(0.2)
+    print("Recording Started... Press Space to Stop.")
+    listener = Listener(on_press=keyPressed)
+    listener.start()
 
-print("Recording Started... Press Space to Stop.")
-listener = Listener(on_press=keyPressed)
-listener.start()
-
-while True:
-    try:
-        data = stream.read(CHUNK, exception_on_overflow=False)
-        frames.append(data)
-    except KeyboardInterrupt:
-        break
-    # if keyboard.is_pressed('space'):
-    #     print("Stopping recording.")
-    #     time.sleep(0.2)
-    #     break
-    if stop_flag:
-        break
+    while True:
+        try:
+            data = stream.read(CHUNK, exception_on_overflow=False)
+            frames.append(data)
+        except KeyboardInterrupt:
+            break
+        if stop_AudioFlag:
+            break
 
 
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-waveFile = wave.open(OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+    waveFile = wave.open(OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
+if __name__ == "__main__":
+    start_AudioRecording()
--- screen.py
+++ screen.py
@@ -2,73 +2,77 @@
 import pyautogui
 import cv2
 import numpy as np
-import sys
-
 from pynput.keyboard import Key, Listener
 
-stop_flag = False
-def keyPressed(key):
+stop_ScreenFlag = False
+
+def ScreenkeyPressed(key):
     if key == Key.space:
         print("Recording Stopped.")
-        global stop_flag 
-        stop_flag = True
+        global stop_ScreenFlag 
+        stop_ScreenFlag = True
         return False
 
-# Specify resolution (the wrong resolution may cause error so choose according to your screen)
-resolution = (3024, 1964)
+def star_ScreenRecording():
+    
+    # Specify resolution (the wrong resolution may cause error so choose according to your screen)
+    resolution = (3024, 1964)
 
-# Specify video codec
-codec = cv2.VideoWriter_fourcc(*"mp4v")
+    # Specify video codec
+    codec = cv2.VideoWriter_fourcc(*"mp4v")
 
-# Specify name of Output file
-filename = "Recording.mp4"
+    # Specify name of Output file
+    filename = "Recording.mp4"
 
-# Specify frames rate.
-fps = 5.0
+    # Specify frames rate.
+    fps = 5.0
 
-# Creating a VideoWriter object
-out = cv2.VideoWriter(filename, codec, fps, resolution)
+    # Creating a VideoWriter object
+    out = cv2.VideoWriter(filename, codec, fps, resolution)
 
-# # optional: To display the recording screen uncomment below code
-# # Create an Empty window
-# cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+    # # optional: To display the recording screen uncomment below code
+    # # Create an Empty window
+    # cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
-# # Resize this window
-# cv2.resizeWindow("Live", 480, 270)
+    # # Resize this window
+    # cv2.resizeWindow("Live", 480, 270)
 
-print("Recording Started... Press Space to Stop.")
-listener = Listener(on_press=keyPressed)
-listener.start()
+    print("Recording Started... Press Space to Stop.")
+    listener = Listener(on_press=ScreenkeyPressed)
+    listener.start()
 
-while True:
-    try:
-        # Take screenshot using PyAutoGUI
-        img = pyautogui.screenshot()
+    while True:
+        try:
+            # Take screenshot using PyAutoGUI
+            img = pyautogui.screenshot()
 
-        # Convert the screenshot to a numpy array
-        frame = np.array(img)
+            # Convert the screenshot to a numpy array
+            frame = np.array(img)
 
-        # Convert it from BGR(Blue, Green, Red) to
-        # RGB(Red, Green, Blue)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Convert it from BGR(Blue, Green, Red) to
+            # RGB(Red, Green, Blue)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Write it to the output file
-        out.write(frame)
+            # Write it to the output file
+            out.write(frame)
 
-        # # Optional: Display the recording screen
-        # cv2.imshow('Live', frame)
+            # # Optional: Display the recording screen
+            # cv2.imshow('Live', frame)
 
-        cv2.waitKey(130)
+            cv2.waitKey(130)
 
-        if stop_flag:
+            if stop_ScreenFlag:
+                break
+
+        except KeyboardInterrupt:
             break
 
-    except KeyboardInterrupt:
-        break
 
+    # Release the Video writer
+    out.release()
 
-# Release the Video writer
-out.release()
+    # Destroy all windows
+    cv2.destroyAllWindows()
 
-# Destroy all windows
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    star_ScreenRecording()
